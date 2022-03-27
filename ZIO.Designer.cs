9ee27f93d726e8f0216bbf116301ceeefc6cafd8
Šimon Novák <82063567+sajmonekk191@users.noreply.github.com>
2022-03-27 22:48:33 +0200

    Add files via upload

--- ZIO.Designer.cs
+++ ZIO.Designer.cs
@@ -31,10 +31,37 @@ namespace ZIO
         {
             this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ZIO));
+            this.Champion = new System.Windows.Forms.PictureBox();
             this.GameTimer = new System.Windows.Forms.Timer(this.components);
-            this.Playerpic = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.Playerpic)).BeginInit();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.ChampHPlbl = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.Goldlbl = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.levellbl = new System.Windows.Forms.Label();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.boosbarlbl = new System.Windows.Forms.Label();
+            this.pictureBox12 = new System.Windows.Forms.PictureBox();
+            this.ammolbl = new System.Windows.Forms.Label();
+            this.pictureBox15 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Champion)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).BeginInit();
             this.SuspendLayout();
+            // 
+            // Champion
+            // 
+            this.Champion.BackColor = System.Drawing.Color.Transparent;
+            this.Champion.Image = global::ZIO.Properties.Resources.player_right;
+            this.Champion.Location = new System.Drawing.Point(343, 584);
+            this.Champion.Name = "Champion";
+            this.Champion.Size = new System.Drawing.Size(47, 74);
+            this.Champion.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Champion.TabIndex = 0;
+            this.Champion.TabStop = false;
             // 
             // GameTimer
             // 
@@ -42,38 +69,169 @@ namespace ZIO
             this.GameTimer.Interval = 1;
             this.GameTimer.Tick += new System.EventHandler(this.GameTimer_Tick);
             // 
-            // Playerpic
+            // pictureBox1
             // 
-            this.Playerpic.BackColor = System.Drawing.Color.Transparent;
-            this.Playerpic.Location = new System.Drawing.Point(360, 167);
-            this.Playerpic.Name = "Playerpic";
-            this.Playerpic.Size = new System.Drawing.Size(75, 100);
-            this.Playerpic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.Playerpic.TabIndex = 0;
-            this.Playerpic.TabStop = false;
+            this.pictureBox1.Image = global::ZIO.Properties.Resources.Hearth;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // ChampHPlbl
+            // 
+            this.ChampHPlbl.AutoSize = true;
+            this.ChampHPlbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ChampHPlbl.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ChampHPlbl.Location = new System.Drawing.Point(68, 20);
+            this.ChampHPlbl.Name = "ChampHPlbl";
+            this.ChampHPlbl.Size = new System.Drawing.Size(128, 32);
+            this.ChampHPlbl.TabIndex = 7;
+            this.ChampHPlbl.Text = "Player_HP";
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::ZIO.Properties.Resources.Gold;
+            this.pictureBox3.Location = new System.Drawing.Point(12, 68);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox3.TabIndex = 11;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Goldlbl
+            // 
+            this.Goldlbl.AutoSize = true;
+            this.Goldlbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.Goldlbl.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Goldlbl.Location = new System.Drawing.Point(68, 75);
+            this.Goldlbl.Name = "Goldlbl";
+            this.Goldlbl.Size = new System.Drawing.Size(149, 32);
+            this.Goldlbl.TabIndex = 12;
+            this.Goldlbl.Text = "Player_Gold";
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.Image = global::ZIO.Properties.Resources.Level;
+            this.pictureBox4.Location = new System.Drawing.Point(12, 124);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox4.TabIndex = 13;
+            this.pictureBox4.TabStop = false;
+            // 
+            // levellbl
+            // 
+            this.levellbl.AutoSize = true;
+            this.levellbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.levellbl.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.levellbl.Location = new System.Drawing.Point(69, 131);
+            this.levellbl.Name = "levellbl";
+            this.levellbl.Size = new System.Drawing.Size(153, 32);
+            this.levellbl.TabIndex = 14;
+            this.levellbl.Text = "Player_Level";
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(699, 40);
+            this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(486, 22);
+            this.progressBar1.TabIndex = 21;
+            // 
+            // boosbarlbl
+            // 
+            this.boosbarlbl.AutoSize = true;
+            this.boosbarlbl.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.boosbarlbl.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.boosbarlbl.Location = new System.Drawing.Point(901, 12);
+            this.boosbarlbl.Name = "boosbarlbl";
+            this.boosbarlbl.Size = new System.Drawing.Size(99, 21);
+            this.boosbarlbl.TabIndex = 22;
+            this.boosbarlbl.Text = "BoosBar_lbl";
+            // 
+            // pictureBox12
+            // 
+            this.pictureBox12.Image = global::ZIO.Properties.Resources.Ammo;
+            this.pictureBox12.Location = new System.Drawing.Point(1831, 20);
+            this.pictureBox12.Name = "pictureBox12";
+            this.pictureBox12.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox12.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox12.TabIndex = 24;
+            this.pictureBox12.TabStop = false;
+            // 
+            // ammolbl
+            // 
+            this.ammolbl.AutoSize = true;
+            this.ammolbl.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ammolbl.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.ammolbl.Location = new System.Drawing.Point(1756, 30);
+            this.ammolbl.Name = "ammolbl";
+            this.ammolbl.Size = new System.Drawing.Size(164, 32);
+            this.ammolbl.TabIndex = 25;
+            this.ammolbl.Text = "Pistol_Ammo";
+            // 
+            // pictureBox15
+            // 
+            this.pictureBox15.Image = global::ZIO.Properties.Resources.MediumPlatform;
+            this.pictureBox15.Location = new System.Drawing.Point(-94, 313);
+            this.pictureBox15.Name = "pictureBox15";
+            this.pictureBox15.Size = new System.Drawing.Size(60, 60);
+            this.pictureBox15.TabIndex = 36;
+            this.pictureBox15.TabStop = false;
             // 
             // ZIO
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(914, 600);
-            this.Controls.Add(this.Playerpic);
+            this.BackColor = System.Drawing.Color.MediumTurquoise;
+            this.ClientSize = new System.Drawing.Size(1904, 1041);
+            this.Controls.Add(this.pictureBox15);
+            this.Controls.Add(this.ammolbl);
+            this.Controls.Add(this.pictureBox12);
+            this.Controls.Add(this.boosbarlbl);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.Champion);
+            this.Controls.Add(this.levellbl);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.Goldlbl);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.ChampHPlbl);
+            this.Controls.Add(this.pictureBox1);
+            this.ForeColor = System.Drawing.SystemColors.Control;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "ZIO";
             this.Text = "🌿 ZIO 🌿";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ZIO_FormClosed);
             this.Load += new System.EventHandler(this.ZIO_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.ZIO_KeyDown);
             this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.ZIO_KeyUp);
-            ((System.ComponentModel.ISupportInitialize)(this.Playerpic)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Champion)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox12)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox15)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Timer GameTimer;
-        private System.Windows.Forms.PictureBox Playerpic;
+        public System.Windows.Forms.PictureBox Champion;
+        public System.Windows.Forms.Timer GameTimer;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label ChampHPlbl;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.Label Goldlbl;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.Label levellbl;
+        public System.Windows.Forms.ProgressBar progressBar1;
+        public System.Windows.Forms.Label boosbarlbl;
+        private System.Windows.Forms.PictureBox pictureBox12;
+        private System.Windows.Forms.Label ammolbl;
+        private System.Windows.Forms.PictureBox pictureBox15;
     }
 }
--- ZIO.cs
+++ ZIO.cs
@@ -1,17 +1,22 @@
-﻿using System;
+﻿// GoldAxe Effect + Damage
+
+using System;
 using System.Drawing;
-using System.Runtime.InteropServices;
 using System.Windows.Forms;
-using ZIO.objekty;
+using ZIO.Objekty;
 
 namespace ZIO
 {
     public partial class ZIO : Form
     {
-        bool up = false;
-        bool down = false;
-        bool left = false;
-        bool right = false;
+        public static bool Left = false;
+        public static bool Right = false;
+        public static bool Up = false;
+        public static bool LastPos = false;
+        public static bool BossMax = false;
+
+        int jumpSpeed = 4;
+        int force = 10;
         public ZIO()
         {
             InitializeComponent();
@@ -19,65 +24,272 @@ namespace ZIO
 
         private void ZIO_Load(object sender, EventArgs e)
         {
-            this.WindowState = FormWindowState.Maximized;
-            this.FormBorderStyle = FormBorderStyle.None;
-            this.BackgroundImage = Properties.Resources.background0;
-            Playerpic.Image = Properties.Resources.player_right;
-            Playerpic.Location = Player.Position;
+            LoadComponents();
         }
-
-        private void ZIO_FormClosed(object sender, FormClosedEventArgs e)
-        {
-            Environment.Exit(0);    
-        }
-
-        private void GameTimer_Tick(object sender, EventArgs e)
-        {
-            if(up) Playerpic.Location = new Point(Player.Position.X, Player.Position.Y + 10);
-            if(down) Playerpic.Location = new Point(Player.Position.X, Player.Position.Y - 10);
-            if(right) Playerpic.Location = new Point(Player.Position.X + 10, Player.Position.Y);
-            if(left) Playerpic.Location = new Point(Player.Position.X - 10, Player.Position.Y);
-
-            if (Player.isleft) Playerpic.Image = Properties.Resources.player_left;
-            else Playerpic.Image = Properties.Resources.player_right;
-        }
-
         private void ZIO_KeyDown(object sender, KeyEventArgs e)
         {
-            switch(e.KeyCode)
+            if (e.KeyCode == Keys.A)
             {
-                case Keys.W:
-                    up = true;
-                    break;
-                case Keys.S:
-                    down = true;
-                    break;
-                case Keys.A:
-                    left = true;
-                    break;
-                case Keys.D:
-                    right = true;
-                    break;
+                Left = true;
+                LastPos = false;
+                Champion.Image = Properties.Resources.player_left;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                Right = true;
+                LastPos = true;
+                Champion.Image = Properties.Resources.player_right;
+            }
+            if (e.KeyCode == Keys.W && !Up)
+            {
+                Up = true;
+            }
+            if(e.KeyCode == Keys.Space)
+            {
+                if (Objekty.Weapons.SinglePistol.Activated && Player.Ammo > 0)
+                {
+                    Objekty.Ammo.PistolAmmo.FireBullet(this);
+                    Player.Ammo--;
+                    ammolbl.Text = Player.Ammo.ToString();
+                }
+                else if (Objekty.Weapons.Melee.GoldAxe.Activated)
+                {
+                    Objekty.Weapons.Melee.GoldAxe.Attack(this, Champion.Location, LastPos);
+                }
+            }
+            if (e.KeyCode == Keys.Escape)
+            {
+                if (GameTimer.Enabled == true)
+                {
+                    Pause_Game();
+                     
+                }
+                else Resume_Game();
+            }
+            if (e.KeyCode == Keys.R)
+            {
+                RestartLevel();
             }
         }
 
         private void ZIO_KeyUp(object sender, KeyEventArgs e)
         {
-            switch (e.KeyCode)
+            if (e.KeyCode == Keys.A)
             {
-                case Keys.W:
-                    up = false;
-                    break;
-                case Keys.S:
-                    down = false;
-                    break;
-                case Keys.A:
-                    left = false;
-                    break;
-                case Keys.D:
-                    right = false;
-                    break;
+                Left = false;
             }
+            if (e.KeyCode == Keys.D)
+            {
+                Right = false;
+            }
+            if (Up)
+            {
+                Up = false;
+            }
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            // Move //
+            if (Up && force < 0)
+            {
+                Up = false;
+            }
+
+            if (Left)
+            {
+                Champion.Left -= 5;
+                if(Champion.Location.X < 20) Left = false;  
+            }
+
+            if (Right)
+            {
+                Champion.Left += 5;
+                if (Champion.Location.X > 1880) Right = false;
+            }
+
+            if (Up)
+            {
+                jumpSpeed = -15;
+                force -= 1;
+            }
+            else
+            {
+                jumpSpeed = 10;
+            }
+            Champion.Top += jumpSpeed;
+            // Move //
+
+            foreach (Control i in this.Controls)
+            {
+                if(i is PictureBox && i.Tag == "Platform")
+                {
+                    if (Champion.Bounds.IntersectsWith(i.Bounds) && !Up)
+                    {
+                        force = 10;
+                        Champion.Top = i.Top - Champion.Height - 1;
+                    }
+                }
+                if (i is PictureBox && i.Tag == "Gold")
+                {
+                    if (Champion.Bounds.IntersectsWith(i.Bounds))
+                    {
+                        i.Dispose();
+                        Player.Gold++;
+                        Goldlbl.Text = Player.Gold.ToString();
+                    }
+                }
+                if (i is PictureBox)
+                {
+                    switch(i.Tag)
+                    {
+                        case "Pistol":
+                            if (Champion.Bounds.IntersectsWith(i.Bounds) && !Objekty.Weapons.SinglePistol.Activated)
+                            {
+                                scripts.Deactive.DeactiveAllWeapons(this);
+                                Objekty.Weapons.SinglePistol.GetPistol();
+                                i.Tag = "EquipItem";
+                                RefreshStats();
+                            }
+                            break;
+                        case "GoldAxe":
+                            if (Champion.Bounds.IntersectsWith(i.Bounds) && !Objekty.Weapons.Melee.GoldAxe.Activated)
+                            {
+                                scripts.Deactive.DeactiveAllWeapons(this);
+                                Objekty.Weapons.Melee.GoldAxe.GetItem();
+                                i.Tag = "EquipItem";
+                                RefreshStats();
+                            }
+                            break;
+                    }
+                }
+                if (i is PictureBox && i.Tag == "Boss")
+                {
+                    Objekty.Enemy.BigToast.pos = i.Bounds;
+                    // Boss Move //
+                    if (Objekty.Enemy.BigToast.pos.X >= 290 && !BossMax)
+                    {
+                        i.Left -= 5;
+                    }
+                    if (Objekty.Enemy.BigToast.pos.X == 289)
+                    {
+                        BossMax = true;
+                    }
+                    if (BossMax)
+                    {
+                        i.Left += 5;
+                        if (Objekty.Enemy.BigToast.pos.X >= 540)
+                        {
+                            BossMax = false;
+                        }
+                    }
+                    // Boss Move //
+
+                    if (Objekty.Enemy.BigToast.HP <= 0)
+                    {
+                        Objekty.Enemy.BigToast.RemoveMob(this, (PictureBox)i);
+                    }
+                    else
+                    {
+                        progressBar1.Value = Objekty.Enemy.BigToast.HP;
+                    }
+                }
+                if (i is PictureBox && i.Tag == "bullet")
+                {
+                    if (LastPos) i.Left += 50;
+                    else i.Left -= 50;
+
+                    if (Objekty.Enemy.BigToast.pos.IntersectsWith(i.Bounds) && Objekty.Enemy.BigToast.HP > 0)
+                    {
+                        Objekty.Enemy.BigToast.HP -= Objekty.Weapons.SinglePistol.Damage;
+                        Objekty.Ammo.PistolAmmo.RemoveBullet((PictureBox)i, this);
+                    }
+                    if(i.Location.X > 1920 || i.Location.X < 0) Objekty.Ammo.PistolAmmo.RemoveBullet((PictureBox)i, this);
+                }
+                if (i is PictureBox && i.Tag == "LuckyBlock")
+                {
+                    if (Champion.Bounds.IntersectsWith(i.Bounds))
+                    {
+                        Objekty.Other.LuckyBlock.RemoveBlock(this, (PictureBox)i);
+                        Objekty.Effekty.OpenEffect.SpawnEffect(this, i.Location);
+                        Objekty.Weapons.Melee.GoldAxe.SpawnItem(this, i.Location);
+                    }
+                }
+            }
+            // Move //
+
+            // Death Func //
+            if (Champion.Location.Y > 1000)
+            {
+                Champion.Location = Player.SpawnPos;
+                if(Player.HP > 0)
+                {
+                    Player.HP--;
+                    ChampHPlbl.Text = Player.HP.ToString();
+                }
+            }
+            if(Champion.Location.X > 1600)
+            {
+                
+            }
+            // Death Func //
+            if(Player.HP <= 0)
+            {
+                Pause_Game();
+            }
+            // Items //
+            if(Objekty.Weapons.SinglePistol.Activated || Objekty.Weapons.Melee.GoldAxe.Activated)
+            {
+                scripts.Items.MoveWithWeapon(this);
+            }
+            // Items //
+        }
+
+        private void ZIO_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Environment.Exit(0);
+        }
+        private void Pause_Game()
+        {
+            GameTimer.Stop();
+        }
+        private void Resume_Game()
+        {
+            GameTimer.Start();
+        }
+        private void RestartLevel()
+        {
+            GameTimer.Dispose();
+            Objekty.Weapons.SinglePistol.Activated = false;
+            Controls.Clear();
+            InitializeComponent();
+            LoadComponents();
+        }
+        private void RefreshStats()
+        {
+            ChampHPlbl.Text = Player.HP.ToString();
+            Goldlbl.Text = Player.Gold.ToString();
+            levellbl.Text = Player.LVL.ToString();
+            ammolbl.Text = Player.Ammo.ToString();
+        }
+        private void LoadComponents()
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.Location = new Point(Screen.PrimaryScreen.Bounds.Width - Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height - Screen.PrimaryScreen.Bounds.Height);
+            this.Size = new Size(Screen.PrimaryScreen.Bounds.Width, Screen.PrimaryScreen.Bounds.Height);
+            this.TopMost = true;
+            Player.SpawnPos = new Point(343, 528);
+            Player.HP = 100;
+            Player.Gold = 0;
+            Player.LVL = 1;
+            Player.Ammo = 0;
+            progressBar1.Value = Objekty.Enemy.BigToast.HP;
+            RefreshStats();
+            LoadLevel();
+        }
+        private void LoadLevel()
+        {
+            Levels.Level1.LoadLevel(this);
         }
     }
 }
